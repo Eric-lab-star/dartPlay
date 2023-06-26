1792fc863c209b452d89850dc1342bf8ac2ae6fe
kimkyungsub <cyon256@icloud.com>
2023-06-26 17:27:41 +0900

    late variable

--- language.dart
+++ language.dart
@@ -1,5 +1,8 @@
-// Null safety
+//Late variables
 
-//when you specity type, you can control whether the type allows null.
-String? a; // Nullable type. Can be 'null'
-String b; // Non-nullable type. cannot be 'null' must be intialized
+late String description;
+
+void main() {
+  description = 'Feijoada!';
+  print(description);
+}
