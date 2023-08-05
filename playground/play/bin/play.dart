84823670af785f3e80b9bcea3d68a7dba023bfe5
kimkyungsub <cyon256@icloud.com>
2023-08-05 19:10:08 +0900

    dart

---
+++ playground/play/bin/play.dart
@@ -0,0 +1,5 @@
+import 'package:play/play.dart' as play;
+
+void main(List<String> arguments) {
+  print('Hello world: ${play.calculate()}!');
+}
---
+++ playground/play/test/play_test.dart
@@ -0,0 +1,8 @@
+import 'package:play/play.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
